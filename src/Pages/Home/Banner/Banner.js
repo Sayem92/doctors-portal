5e522b6093c852ebb9487f3d8b266f63f8536aa3
Sayem92 <sayemmiha123@gmail.com>
2022-11-12 15:45:55 +0600

    add banner bgImage

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import chair from '../../../assets/images/chair.png'
+import bg from '../../../assets/images/bg.png'
 import ButtonPrimary from '../../../components/buttonPrimary/ButtonPrimary';
 
 const Banner = () => {
@@ -7,14 +8,14 @@ const Banner = () => {
    
     return (
         <div 
-        className="hero bg-sky-200 text-black rounded-lg " 
-        // style={{ backgroundImage: `url(${chair})`, backgroundRepeat: 'no-repeat'}}
+        className="hero  text-black rounded-lg " 
+        style={{ backgroundImage: `url(${bg})`}}
         >
 
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} className="lg:w-1/2 rounded-lg shadow-2xl" alt='' />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
 
                         <ButtonPrimary>Get Started</ButtonPrimary>
