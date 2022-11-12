006f4a6b89a6ac7eeee266d458481a8add2e5e2a
Sayem92 <sayemmiha123@gmail.com>
2022-11-12 12:30:50 +0600

    add nabvar , banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div>
-     
+    <div className='max-w-[1440px] mx-auto'>
+
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0FCFEC",
+          secondary : '#19D3AE',
+          accent: "#3A4256", 
+          neutral: "#2E2938",
+          // "base-100": "#37393E",
+         
+
+
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
