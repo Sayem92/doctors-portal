d2e701f78f3409536ac74cef28b3a15c03b18dd9
Sayem92 <sayemmiha123@gmail.com>
2022-11-12 13:29:57 +0600

    add infoCards

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import InfoCards from '../infoCards/InfoCards';
+
 
 const Home = () => {
     return (
         <div className='mx-5'>
             <Banner></Banner>
+         <InfoCards></InfoCards>
         </div>
     );
 };
---
+++ src/Pages/Home/infoCards/InfoCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const InfoCard = ({ card }) => {
+    const { name, description, icon, bgClass } = card;
+
+    return (
+        <div className={`p-6 text-white card card-side shadow-xl ${bgClass}`}>
+            <figure>
+                <img src={icon} alt="Movie" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{description}</p>
+               
+            </div>
+        </div>
+    );
+};
+
+export default InfoCard;
